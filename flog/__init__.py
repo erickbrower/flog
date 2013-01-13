117d89ab39285b8022a7565d98be04d8353210e6
Erick Brower <cerickbrower@gmail.com>
2013-01-12 23:30:01 -0500

    Cleaned up references to MongoEngine and updated pip requirements.txt

--- flog/__init__.py
+++ flog/__init__.py
@@ -1,11 +1,6 @@
 from flask import Flask
-from flask.ext.mongoengine import MongoEngine
 
 app = Flask('flog')
-app.config['MONGODB_DB'] = 'flog_dev'
-app.config['SECRET_KEY'] = 'd0ntp4nic'
-
-db = MongoEngine(app)
 
 from flog.api_views import api
 app.register_blueprint(api, url_prefix='/api')
